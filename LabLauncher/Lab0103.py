ee042d8e7b4a51e6ba6e7c4a213ad0f90e558266
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-02-08 12:36:00 +0300

    Update Lab0103.py

--- LabLauncher/Lab0103.py
+++ LabLauncher/Lab0103.py
@@ -1,3 +1,27 @@
 def launch():
-    global a
-    a = 2 + 4
+    n = int(input())
+    arr = []
+    for i in range(n):
+        arr.append(input())
+    
+    nums = set()
+    
+    for i in range(n):
+        nums.add(arr[i])
+    
+    print("Unique elements:", nums)
+    print("The quantity of unique elements:", len(nums))
+    
+    maxx = 0
+
+    for i in range(n):
+        for j in range(n):
+            if arr[i] == arr[j] and i != j:
+                maxx = arr[i]
+                break
+
+    for i in range(n):
+        for j in range(n):
+            if arr[i] == arr[j] and i != j and maxx < arr[i]:
+                maxx = arr[i]
+    print("Max duplicate:", maxx)
